0ce6547a697aedd94d33502d282c8fd4c1ace08d
Nada Hosny <nadahosni96@gmail.com>
2022-11-29 13:30:40 -0800

    fixed negative

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,7 +5,7 @@ def print_last_digit(number):
         print(last_digit, end="")
         return (last_digit)
     elif (number < 0):
-        last_digit = number % -10
+        last_digit = abs(number) % 10
         print(last_digit, end="")
         return (last_digit)
     else:
