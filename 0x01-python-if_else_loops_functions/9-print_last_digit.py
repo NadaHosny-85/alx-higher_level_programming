bb144d10df5c599e2cc684c5a0ada557bbb13693
Nada Hosny <nadahosni96@gmail.com>
2022-11-29 13:27:06 -0800

    added 10th task

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last_digit = 0
+    if (number == 0):
+        print(last_digit)
+        return (last_digit)
+    elif (number < 0):
+        last_digit = number % -10
+        print(last_digit)
+        return (last_digit)
+    else:
+        last_digit = number % 10
+        print(last_digit)
+        return (last_digit)
