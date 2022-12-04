baa5e09225c3def45c687ca873690f1ccdf6b0b5
Nada Hosny <nadahosni96@gmail.com>
2022-12-04 14:14:32 -0800

    fixed conditional operator

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     str_len = len(my_string)
     modified_str = ""
     for i in str_len - 1:
-        if (my_string[i] == 'c' || my_string[i] == 
+        if (my_string[i] == 'c' or my_string[i] == 
             'C'):
             continue
         else:
