822e3f223ffc511f82c8acf12c243e73c57d2325
Nada Hosny <nadahosni96@gmail.com>
2022-12-04 14:17:45 -0800

    changed condition

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,9 +3,7 @@ def no_c(my_string):
     str_len = len(my_string)
     modified_str = ""
     for i in (str_len):
-        if (my_string[i] == 'c' or my_string[i] == 
+        if (my_string[i] != 'c' and my_string[i] != 
             'C'):
-            continue
-        else:
             modified_str += my_string[i]
     return (modified_str)
