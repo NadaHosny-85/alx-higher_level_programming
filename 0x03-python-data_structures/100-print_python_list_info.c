07e6715011b21e4b642a4448a5ade93da7efb748
Nada Hosny <nadahosni96@gmail.com>
2022-12-05 06:53:49 -0800

    added bonus advanced task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <python.h>
+#include <object.h>
+#include <listobject.h>
+
+/**
+ * print_python_list - function prints info
+ * about a python list
+ *
+ * @p: pyObject
+ *
+ * return: void
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int i;
+
+	long int size = PyList_Size(p);
+
+	PyListObject *list = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", list->allocated);
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %i: %s\n", i, Py_TYPE(list->ob_item[i])->tp_name);
+	}
+
+
