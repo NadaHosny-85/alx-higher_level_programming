2e070d5b24d0cbd1e607ad802da5f3c93e5a7a97
Nada Hosny <nadahosni96@gmail.com>
2022-12-05 05:13:43 -0800

    added a fix to task 6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     str_len = len(my_string)
     modified_str = ""
-    for i in (str_len):
+    for i in range(str_len):
         if (my_string[i] != 'c' and my_string[i] != 
             'C'):
             modified_str += my_string[i]
