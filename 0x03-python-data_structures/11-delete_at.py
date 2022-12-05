49f647ac9f2a83018b319672ad0586bb74ca09ea
Nada Hosny <nadahosni96@gmail.com>
2022-12-05 06:02:07 -0800

    fixed code  style for task 12

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-   list_len = len(my_list)
-   if (idx > list_len - 1):
-       return (my_list)
-   elif (idx < 0):
-       return (my_list)
-   else:
-       del(my_list[idx])
-       return (my_list)
+    list_len = len(my_list)
+    if (idx > list_len - 1):
+        return (my_list)
+    elif (idx < 0):
+        return (my_list)
+    else:
+        del(my_list[idx])
+        return (my_list)
