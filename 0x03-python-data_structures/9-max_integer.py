ba3b0b400ac921ee6fe65013bfd38287e4a31d54
Nada Hosny <nadahosni96@gmail.com>
2022-12-05 04:48:17 -0800

    added condition for negative

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,8 @@ def max_integer(my_list=[]):
     max_num = 0
     if (list_len == 0):
         return (None)
+    else:
+        max_num = my_list[0]
     for num in my_list:
         if (num > max_num):
             max_num = num
