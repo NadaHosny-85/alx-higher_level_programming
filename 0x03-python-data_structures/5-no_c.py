ea5b7daf2e263df6e2a077ce0d1d12756571bc72
Nada Hosny <nadahosni96@gmail.com>
2022-12-05 05:20:14 -0800

    fixed code style for task 6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     str_len = len(my_string)
     modified_str = ""
     for i in range(str_len):
-        if (my_string[i] != 'c' and my_string[i] != 
-            'C'):
+        if ((my_string[i] != 'c')
+                and (my_string[i] != 'C')):
             modified_str += my_string[i]
     return (modified_str)
