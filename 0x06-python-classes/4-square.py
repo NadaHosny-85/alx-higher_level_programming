5009d196c9253fe63ca805f823246490f71a572d
Nada Hosny <nadahosni96@gmail.com>
2022-12-20 02:27:51 -0800

    added task 5

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""this is a square class with its own attributes"""
+
+
+class Square():
+    """class square with its attribute size which must be an integer"""
+
+    def __init__(self, size=0):
+        """initializing the class"""
+
+        self.__size = size
+
+    def size(self):
+        """instance returns size itself"""
+
+        return self.__size
+
+    def size(self, value):
+        """set a new value for size and check on its if conditions"""
+
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns area of the square"""
+
+        return self.__size ** 2
