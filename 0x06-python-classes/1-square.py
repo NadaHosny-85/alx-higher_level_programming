ce2ccddadf43edb79a431ef4552971547cb27503
Nada Hosny <nadahosni96@gmail.com>
2022-12-20 01:55:21 -0800

    added second task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""this is class square which has
+methods such as size"""
+class Square():
+    """class representing the square"""
+
+    def __init__(self, size):
+        """intializing the class and its attributes"""
+        self.__size = size
